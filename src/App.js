d8f0238790e160881aa202dd5aef3c01f14c5313
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-09 11:51:03 +0530

    removed the scroll bar from list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const NewHeader = () => {
-	const [presentDate, setPresentDate] = useState(new Date());
-	const date = new Date();
-	const month = date.getMonth();
-	const days = [];
+const getDays = () => {
+	let date = new Date();
+	let month = date.getMonth();
+	let days = [];
 	while (date.getMonth() === month) {
 		days.push(new Date(date).getDate());
 		date.setDate(date.getDate() + 1);
 	}
+	return days;
+};
+
+const NewHeader = () => {
+	const [presentDate, setPresentDate] = useState(new Date());
+	const month = presentDate.getMonth();
+	const days = getDays();
 	const handleDateClick = (e) => {
 		setPresentDate(
 			new Date(presentDate.getFullYear(), month, e.currentTarget.id)
@@ -21,10 +27,15 @@ const NewHeader = () => {
 			<h1 className="container">Shop List</h1>
 			<p className="container">Date: {presentDate.toDateString()}</p>
 			<div>
-				<ul className="days">
+				<ul className="days" id="days">
 					{days.map((day, index) => {
 						return (
-							<li id={day} key={index} className="" onClick={handleDateClick}>
+							<li
+								id={day}
+								key={index}
+								className="day"
+								onClick={handleDateClick}
+							>
 								{day}
 							</li>
 						);
