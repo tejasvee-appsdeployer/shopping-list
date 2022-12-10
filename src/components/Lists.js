e118d5ad8a5f90c6d49ecd89283120576ebe1613
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-10 16:16:31 +0530

    added remove button

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -18,6 +18,11 @@ function Lists({ items, setItems, presentDate }) {
 		setUserInput("");
 	};
 
+	const deleteItem = (e) => {
+		const id = e.currentTarget.id;
+		setItems(items.filter((item) => item.id === id));
+	};
+
 	return (
 		<div className="container mt-4">
 			<div className="input-group mb-3">
@@ -56,6 +61,22 @@ function Lists({ items, setItems, presentDate }) {
 								<div className="form-control" id="item-input" disabled>
 									{item.name}
 								</div>
+								<button
+									id={item.name}
+									className="btn btn-outline-danger"
+									onClick={deleteItem}
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										className="bi bi-x"
+										viewBox="0 0 16 16"
+									>
+										<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+									</svg>
+								</button>
 							</li>
 						);
 					})}
